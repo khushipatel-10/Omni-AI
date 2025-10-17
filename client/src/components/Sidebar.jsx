b87bf8416101566593cd714abf78bb21dd8af3b9
khushipatel-10 <happipatel10@gmail.com>
2025-10-17 13:10:20 -0500

    Dashboard progress: added cards

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useUser } from '@clerk/clerk-react'
 import { useClerk } from '@clerk/clerk-react'
-import { House, Image, Eraser, Scissors, FileText, SquarePen, Hash, Users } from 'lucide-react'
+import { House, Image, Eraser, Scissors, FileText, SquarePen, Hash, Users, LogOut } from 'lucide-react'
 import { NavLink } from 'react-router-dom'
+import { Protect } from '@clerk/clerk-react';
 
 const navItems = [
     { to: '/ai', label: 'Dashboard', Icon: House },
@@ -26,7 +27,7 @@ const Sidebar = ({ sidebar, setSidebar }) => {
             <div className='my-7 w-full'>
                 <img src={user.imageUrl} className='w-12 h-12 rounded-full mx-auto' alt='User image' />
                 <h1 className='mt-1 text-center'>{user.fullName}</h1>
-                <div>
+                <div className='px-6 mt-5 text-sm text-gray-600 font-medium'>
                     {navItems.map(({ to, label, Icon }) => (
                         <NavLink key={to} to={to} end={to === '/ai'} onClick={() =>
                             setSidebar(false)}
@@ -44,7 +45,18 @@ const Sidebar = ({ sidebar, setSidebar }) => {
                     ))}
                 </div>
             </div>
-
+            <div className='w-full border-t border-gray-200 p-4 px-7 flex items-center justify-between'>
+                <div onClick={openUserProfile} className='flex gap-2 items-center cursor-pointer'>
+                    <img src={user.imageUrl} className='w-8 rounded-full' alt="" />
+                    <div>
+                        <h1 className='text-sm font-medium'>{user.fullName}</h1>
+                        <p className='text-xs text-gray-500'>
+                            <Protect plan='premium' fallback="Free">Premium</Protect> Plan
+                        </p>
+                    </div>
+                </div>
+                <LogOut onClick={signOut} className='w-4.5 text-gray-400 hover:text-gray-700 transition cursor-pointer' />
+            </div>
         </div>
     )
 }
--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -14,7 +14,7 @@ const Layout = () => {
   return user ? (
     <div className='flex flex-col items-start justify-start h-screen'>
       <nav className='w-full px-8 min-h-14 flex items-center justify-between border-b border-gray-200'>
-        <img src={assets.logo} alt="Logo" className='h-12 cursor-pointer' onClick={() => navigate('/')} />
+        <img src={assets.logo} alt="Logo" className='w-32 mt-2 mb-2 sm:w-44 cursor-pointer' onClick={() => navigate('/')} />
         {
           sidebar ? <X onClick={() => setSidebar(false)} className='w-6 h-6 text-gray-600 sm:hidden'></X>
             : <Menu onClick={() => setSidebar(true)} className='w-6 h-6 text-gray-600 sm:hidden'></Menu>
