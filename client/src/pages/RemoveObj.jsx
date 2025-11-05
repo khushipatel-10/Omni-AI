8073da0642905c8348fcb086470c0e9d0cf3badd
khushipatel-10 <happipatel10@gmail.com>
2025-11-05 13:50:42 -0600

    RemoveObj UI finished

--- client/src/pages/RemoveObj.jsx
+++ client/src/pages/RemoveObj.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Scissors, Sparkles } from 'lucide-react'
+import { useState } from 'react'
 
 const RemoveObj = () => {
 
   const [input, setInput] = useState('')
+  const [object, setObject] = useState('')
+
 
   const onSubmitHandler = async (e) => {
     e.preventDefault()
@@ -17,8 +21,8 @@ const RemoveObj = () => {
       <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
 
         <div className='flex items-center gap-3'>
-          <Sparkles className='w-6 text-[#FF4938]' />
-          <h1 className='text-xl font-semibold'>Background Removal</h1>
+          <Sparkles className='w-6 text-[#4A7AFF]' />
+          <h1 className='text-xl font-semibold'>Object Removal</h1>
 
         </div>
 
@@ -26,11 +30,13 @@ const RemoveObj = () => {
 
         <input onChange={(e) => setInput(e.target.files[0])} type="file" accept="image/*" className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 text-gray-600' required />
 
-        <p className='text-xs text-gray-500 font-light mt-1'>Supports JPG, PNG, and other image formats</p>
+        <p className='mt-6 text-sm font-medium'>Describe object name to remove</p>
 
-        <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#F6AB41] to-[#FF4938] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
-          <EraserIcon className='w-5' />
-          Remove background
+        <textarea onChange={(e) => setObject(e.target.value)} value={object} rows={4} className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300' placeholder='e.g., watch or spoon, only single object name' required />
+
+        <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#417DF6] to-[#8E37EB] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
+          <Scissors className='w-5' /> 
+          Remove object
         </button>
       </form>
 
@@ -39,13 +45,13 @@ const RemoveObj = () => {
 
       <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96'>
         <div className='flex items-center gap-3'>
-          <EraserIcon className='w-5 h-5 text-[#FF4938]' />
+          <Scissors className='w-5 h-5 text-[#4A7AFF]' />
           <h1 className='text-xl font-semibold'>Processed image</h1>
         </div>
         <div className='flex-1 flex justify-center items-center'>
           <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
-            <EraserIcon className='w-9 h-9' />
-            <p>Upload an image and click "Remove background" to get started</p>
+            <Scissors className='w-9 h-9' />
+            <p>Upload an image and click "Remove Object" to get started</p>
           </div>
         </div>
 
